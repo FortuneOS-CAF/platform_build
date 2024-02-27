3248e94dbe4fb468ab910cd7a634a14b962dbcb3
dianlujitao <dianlujitao@lineageos.org>
2024-02-27 10:07:14 +0530

    product: Exclude AOSP APN list FortuneOS builds
    Change-Id: I1fd10089421a2e4b57ef565d87ad1f6036bd1898

--- target/product/aosp_product.mk
+++ target/product/aosp_product.mk
@@ -39,5 +39,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony:
 #   Provide a APN configuration to GSI product
+ifeq ($(FORTUNE_BUILD),)
 PRODUCT_COPY_FILES += \
     device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+endif

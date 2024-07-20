AOSPA/android_build

Add roomservice.
Change-Id: Ia17023e1c19695890aee99de998356c9aadc104f

envsetup.sh
@@ -662,6 +662,20 @@ function lunch()
     fi
 
     check_product $product
+    if [ $? -ne 0 ]
+    then
+        # if we can't find a product, try to grab it off the AOSPA github
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/pa/build/tools/roomservice.py $product
+        cd - > /dev/null
+        check_product $product
+    else
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/pa/build/tools/roomservice.py $product true
+        cd - > /dev/null
+    fi
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
@@ -669,6 +683,15 @@ function lunch()
     build_build_var_cache
     if [ $? -ne 0 ]
     then
+        echo
+        echo "** Don't have a product spec for: '$product'"
+        echo "** Do you have the right repo manifest?"
+        product=
+    fi
+
+    if [ -z "$product" -o -z "$variant" ]
+    then
+        echo
         return 1
     fi
 

FortuneOS-CAF/platform_build

envsetup.sh
@@ -834,6 +834,20 @@ function lunch()
     fi
 
     check_product $product
+    if [ $? -ne 0 ]
+    then
+        # if we can't find a product, try to grab it off the FortuneOS github
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/fortune/build/tools/roomservice.py $product
+        cd - > /dev/null
+        check_product $product
+    else
+        T=$(gettop)
+        cd $T > /dev/null
+        vendor/fortune/build/tools/roomservice.py $product true
+        cd - > /dev/null
+    fi
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
@@ -841,6 +855,15 @@ function lunch()
     build_build_var_cache
     if [ $? -ne 0 ]
     then
+        echo
+        echo "** Don't have a product spec for: '$product'"
+        echo "** Do you have the right repo manifest?"
+        product=
+    fi
+
+    if [ -z "$product" -o -z "$variant" ]
+    then
+        echo
         if [[ "$product" =~ .*_(eng|user|userdebug) ]]
         then
             echo "Did you mean -${product/*_/}? (dash instead of underscore)"
7174d872a5902812855cb0fb84ca9c7c16e0a2b3
Jyotiraditya Panda <jyotiraditya@aospa.co>
2024-09-22 02:58:30 +0530

    envsetup: Add support for barista
    Also included:
    
      Author: Gokul GK <gokulkgm@users.noreply.github.com>
      Date: 2023-01-24 21:44:20 +0530
    
        envsetup.sh: source vendorsetup.sh files again after roomservice sync
    
        Product repos grabbed by roomservice may contain vendorsetup.sh
        files that needs to be executed before build, so once again find
        vendorsetup.sh files and execute the contents after sync
    
        Change-Id: I30cfa42cad7cba8b81b8430fec8a8468733a1db9
    
    Change-Id: I763aaf1ad1d032d79bf543bd06971f088bb6c1b3
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- envsetup.sh
+++ envsetup.sh
@@ -512,6 +512,10 @@ function lunch()
         return 1
     fi
 
+    local T=$(gettop)
+    $T/prebuilts/build-tools/linux-x86/bin/py3-cmd $T/vendor/fortune/build/tools/barista.py $product
+    source_vendorsetup &>/dev/null
+
     if (echo -n $product | grep -q -e "^fortune_") ; then
         FORTUNE_BUILD=$(echo -n $product | sed -e 's/^fortune_//g')
     else
